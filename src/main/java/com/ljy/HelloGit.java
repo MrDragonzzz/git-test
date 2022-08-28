4f5e8de613bb4c17a43c10c4c755ec846eeefbb3
ljy <ljy@qq.com>
2022-08-28 11:37:40 +0800

    push-commit

--- src/main/java/com/ljy/HelloGit.java
+++ src/main/java/com/ljy/HelloGit.java
@@ -6,5 +6,6 @@ public class HelloGit {
         System.out.println("hello github!");
         System.out.println("hello git3");
         System.out.println("hello git2!");
+        System.out.println("push test");
     }
 }
